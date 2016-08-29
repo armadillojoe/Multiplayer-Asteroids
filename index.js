17387c3a8dc93767334eaf76999bb0d6d5aa532a
Joe Joe <armadillojoe1@gmail.com>
2016-08-28 17:37:57 -0700

    port issues

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-/* 
+/*
  * Copyright 2016 Joseph Jimenez
- * Server side code for simple 2d network 
+ * Server side code for simple 2d network
  * enabled game.
  */
- 
+
 (function() {
 	"use strict";
-	
+
 	// Import all needed modules
 	let express = require('express');
 	let app = express();
 	let http = require("http").Server(app);
 	let io = require("socket.io")(http);
-	
+
 	// Constants
-	const PORT = 7777;
+	const PORT = process.env.PORT || 7777;
 	const HEIGHT = 600;
 	const WIDTH = 800;
-	
+
 	// Stores all players currently connected
 	let players = {};
-	
+
 	// Listen on the specified port
 	http.listen(PORT, function() {
 		console.log("Listening on port %d!", PORT);
 	});
-	
+
 	// Allow serving static files
 	app.use(express.static(__dirname + "/public"));
-	
+
 	// Serve up the root path index.html
 	app.get("/", function(req, res) {
 		res.sendFile(__dirname + "/public/index.html");
 	});
-	
+
 	// Handle client connections
 	io.on("connection", function(socket) {
 		let newPlayer = {
@@ -51,7 +51,7 @@
 			angle: newPlayer.angle
 		};
 		console.log("New user connected! Total Users: %d", Object.keys(players).length);
-		
+
 		// Send all other players to the new player
 		socket.on("requestPlayers", function(name) {
 			let toSend = {
@@ -65,7 +65,7 @@
 			socket.broadcast.emit("newPlayer", newPlayer);
 			io.to(socket.id).emit("requestPlayers", toSend);
 		});
-		
+
 		// Update players position coordinates
 		socket.on("updatePlayer", function(position) {
 			if (players.hasOwnProperty(socket.id)) {
@@ -80,7 +80,7 @@
 				});
 			}
 		});
-		
+
 		// Handles shots being fired
 		socket.on("shotFired", function() {
 			let rad = players[socket.id].angle * Math.PI / 180;
@@ -90,12 +90,12 @@
 				dir: players[socket.id].angle
 			});
 		});
-		
+
 		// Tells all clients when a ship has been shotFired
 		socket.on("shipHit", function() {
 			socket.broadcast.emit("shipHit", socket.id);
 		});
-		
+
 		// Handle disconnections
 		socket.on("disconnect", function() {
 			socket.broadcast.emit("removePlayer", socket.id);
